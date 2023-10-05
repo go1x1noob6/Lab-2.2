29b21d8881eb64cb37e046eb1a406f30bda06a69
user <user@DESKTOP-BFFI4GQ>
2023-10-05 12:09:01 +0700

    Добавьте файлы проекта.

---
+++ WindowsProject1/WindowsProject1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+const double PI = 3.141592;
+
+int main()
+{
+	double y;
+	double a, b;
+	cin >> a >> b;
+	y = (sqrt(pow(b, 2) - pow(a, 2)));
+	cout << y;
+	return 0;
+}
+ 
+
